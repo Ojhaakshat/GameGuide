27829967c998ee9b0d724d57bee53159b36cf195
Ojhaakshat <ojhaakshat123@gmail.com>
2021-03-16 12:39:18 +0530

    SignUP Login Logout And MaintainingStaus

---
+++ scripts/auth.js
@@ -0,0 +1,52 @@
+const signupform = document.querySelector('#signup-form');
+const loginform = document.querySelector('#login-form');
+
+
+//signup
+signupform.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get info 
+    const email = signupform['signup-email'].value;
+    const password = signupform['signup-password'].value;
+    //signup user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupform.reset();
+    })
+})
+
+//signout or logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        console.log('Succesfully Signed Out');
+    })
+})
+
+//login
+loginform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //get info
+    const email = loginform['login-email'].value;
+    const password = loginform['login-password'].value;
+    //login user
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // console.log(cred.user);
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginform.reset();
+    })
+})
+
+//Auth status changes
+auth.onAuthStateChanged((user) => {
+    if(user) {
+        console.log('user logged in: ', user);
+    } else {
+        console.log('user logged out');
+    }
+})
